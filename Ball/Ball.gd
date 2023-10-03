296398c0832057a7a8268d53be43961d2618ce7b
Joshuamom <jmom@iu.edu>
2023-10-03 18:39:39 -0400

    things changes

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -4,8 +4,20 @@ var min_speed = 100.0
 var max_speed = 600.0
 var speed_multiplier = 1.0
 var accelerate = false
-
+var decay = 0.04
 var released = true
+
+var time_highlight = 0.4
+var time_highlight_size = 0.3
+var wobble_period = 0.0
+var wobble_amplitude = 0.0
+var wobble_max = 5
+var wobble_direction = Vector2.ZERO
+var decay_wobble = 0.15
+var tween
+var distort_effect = 0.0002
+
+
 
 var initial_velocity = Vector2.ZERO
 
@@ -24,6 +36,16 @@ func _on_Ball_body_entered(body):
 	if body.has_method("hit"):
 		body.hit(self)
 		accelerate = true	
+	if tween:
+		tween.kill()
+	tween = create_tween().set_parallel(true)
+	$Ball/highlight.modulate.a = 1.0
+	tween.tween_property($Ball/highlight, "modulate:a", 0, time_highlight).set_trans(Tween.TRANS_LINEAR)
+	$Ball/highlight.scale = Vector2(2.0,2.0)
+	tween.tween_property($Ball/highlight, "scale", Vector2(1.0,1.0),time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	wobble_direction = linear_velocity.orthogonal().normalized()
+	wobble_amplitude = wobble_max
+	$Ball/highlight.modulate.a = 1.0
 
 func _input(event):
 	if not released and event.is_action_pressed("release"):
@@ -47,9 +69,12 @@ func _integrate_forces(state):
 		state.linear_velocity.y = sign(state.linear_velocity.y) * min_speed * speed_multiplier
 	if state.linear_velocity.length() > max_speed * speed_multiplier:
 		state.linear_velocity = state.linear_velocity.normalized() * max_speed * speed_multiplier
+	if $Ball/highlight.modulate.a > 0:
+		$Ball/highlight.modulate.a -= decay
+
 
 func change_size(s):
-	$ColorRect.scale = s
+	$Ball.scale = s
 	$CollisionShape2D.scale = s
 
 func change_speed(s):
@@ -57,3 +82,17 @@ func change_speed(s):
 
 func die():
 	queue_free()
+	
+func wobble():
+	wobble_period += 1
+	if wobble_amplitude > 0:
+		var pos = wobble_direction * wobble_amplitude * sin(wobble_period)
+		$Images.position = pos
+	wobble_amplitude -= decay_wobble
+		
+		
+func distort():
+	var direction = Vector2(1 + linear_velocity.length() * distort_effect, 1 - linear_velocity.length() * distort_effect)
+	$Images.rotation = linear_velocity.angle()
+	$Images.scale = direction
+
--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -5,6 +5,10 @@ var speed = 10.0
 var width = 0
 var width_default = 0
 var decay = 0.02
+var tween
+var time_highlight = 0.4
+var time_highlight_size = 0.3
+var tween2
 
 func _ready():
 	width = $CollisionShape2D.get_shape().size.x
@@ -16,13 +20,27 @@ func _physics_process(_delta):
 	position = target
 	for c in $Powerups.get_children():
 		c.payload()
+	if $paddel/highlight.modulate.a > 0:
+		$paddel/highlight.modulate.a -= decay
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		target.x += event.relative.x
 
 func hit(_ball):
-	pass
+	$paddel/highlight.modulate.a = 1.0
+	if tween:
+		tween.kill()
+	tween = create_tween().set_parallel(true)
+	$paddel/highlight.modulate.a = 1.0
+	tween.tween_property($paddel/highlight, "modulate:a", 0.0, time_highlight)
+	$paddel/highlight.scale = Vector2(1.5, 1.5)
+	tween2 = create_tween().set_parallel(true)
+	print(tween)
+	#tween2.set_trans(Tween.TRANS_BOUNCE)
+	#tween2.set_ease(Tween.EASE_IN)
+	tween2.tween_property($paddel/hightlight, "scale", Vector2(1.0,1.0), time_highlight_size)#.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+
 
 func powerup(payload):
 	for c in $Powerups.get_children():
