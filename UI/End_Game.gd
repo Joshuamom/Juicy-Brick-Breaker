9c56c4931d663980d4f7c71f3013fdc7eb1a1be8
Joshuamom <jmom@iu.edu>
2023-10-11 17:52:29 -0400

    now done finally

--- UI/End_Game.gd
+++ UI/End_Game.gd
@@ -1,4 +1,8 @@
 extends Control
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Game.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
